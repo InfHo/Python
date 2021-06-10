2358274af551e6baffe8f14ecac4d77d31685c84
InfHo <75042725+InfHo@users.noreply.github.com>
2021-06-10 15:38:59 +0200

    Add files via upload

--- Programme/QR_Code_erstellen.py
+++ Programme/QR_Code_erstellen.py
@@ -1,7 +1,11 @@
+# importiere QR Modul
 import qrcode
 
+# zu verschlüsselnde Daten
 data = "https://infho.eu/"
 
+# verschlüssele Daten mit der make-Funktion
 img = qrcode.make(data)
 
+# speichere bild 
 img.save('qr_code_infho.png')
