091997c7f663f113ccee99e38e167f0b1b1604c6
Patrick-InfHo <patrick.haetti@infho.eu>
2022-02-12 15:41:14 +0100

    fstring printer bei basics hinzugefügt

---
+++ Basics/fstring_printer.py
@@ -0,0 +1,20 @@
+
+essen = "Obst"
+
+name = "Joe"
+
+print(name, "isst gerade", essen)
+
+print(f'{name} isst gerade {essen}')
+
+
+import random
+
+zufallszahl = random.randint(1,10)
+
+print(f'Die Lieblingszahl von {name} ist {zufallszahl}')
+
+
+print(f'{zufallszahl} mal {name} ist gleich {zufallszahl*name}')
+
+print(f'{zufallszahl} plus {zufallszahl} ist gleich {zufallszahl+zufallszahl}')
