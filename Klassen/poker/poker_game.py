0d8f4fea82913764bca3f3baeb39057341519ede
Patrick-InfHo <patrick.haetti@infho.eu>
2022-02-12 16:12:40 +0100

    Stapel wurde hinzugefügt

--- Klassen/poker/poker_game.py
+++ Klassen/poker/poker_game.py
@@ -3,7 +3,7 @@
 class Karte:
 
     # erstelle Liste mit Farben und Nummern
-    farben_namen = ["Pik", "Ass", "Herz", "Karo"]
+    farben_namen = ["Pik", "Kreuz", "Herz", "Karo"]
     nummern_namen = [None, 'Ass', '2','3','4','5','6','7','8',
                      '9','10','Bube', 'Dame', 'König']
 
@@ -25,3 +25,43 @@ neue_karte_2 = Karte(3,13)
 # printe die Karten
 print(neue_karte_1)
 print(neue_karte_2)
+
+
+class Stapel:
+
+    def __init__(self):
+        self.karten = list()
+        for farbe in range(4):
+            for nummer in range(1,14):
+                self.karten.append(Karte(farbe, nummer))
+
+    def __str__(self):
+        kv=str()
+        for i in self.karten:
+            kv+=str(i)
+            kv+="\n"
+        return kv
+
+    
+stapel = Stapel()
+
+print(stapel)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
